a92cf0c71212b0e8b2600f20b7c8c48e74aadc0e
tceperosan <tceperosan@educacion.navarra.es>
2025-12-19 08:53:16 +0100

    Constructor añadido

--- Examen_ThaliaCepero/src/Examen/Vehiculo.java
+++ Examen_ThaliaCepero/src/Examen/Vehiculo.java
@@ -1,5 +1,19 @@
 package Examen;
 
 public class Vehiculo {
+	private String matricula;
+	private String marca;
+	private String modelo;
+	private double prestado;
+	
+	public Vehiculo(String matricula, String marca, String modelo, double prestado) {
+		super();
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.prestado = prestado;
+	}
+	
+	
 
 }
