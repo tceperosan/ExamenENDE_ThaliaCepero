d9b357ac1a4089212d71d8c8c71e4b1042ff7adc
tceperosan <tceperosan@educacion.navarra.es>
2025-12-19 09:03:48 +0100

    Métodos añadidos

--- Examen_ThaliaCepero/src/Examen/Vehiculo.java
+++ Examen_ThaliaCepero/src/Examen/Vehiculo.java
@@ -1,5 +1,35 @@
 package Examen;
 
 public class Vehiculo {
-
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public double getPrestado() {
+		return prestado;
+	}
+	public void setPrestado(double prestado) {
+		this.prestado = prestado;
+	}
+	@Override
+	public String toString() {
+		return "Vehiculo [matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo + ", prestado=" + prestado
+				+ "]";
+	}
+	
 }
